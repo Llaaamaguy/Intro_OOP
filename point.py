43db554a45af4b1d548e3c855b6f566b4203d23f
Llaaamaguy <llamaguy.8267@gmail.com>
2020-09-17 14:40:01 -0400

    update point.py

--- point.py
+++ point.py
@@ -17,12 +17,39 @@ class Point:
     def abs(self):
         return sqrt(self.x**2 + self.y**2)
 
+class Rectangle:
+    x = 0.0
+    y = 0.0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def length(self, p, p2):
+        return (p2[2] - p[2]) 
+
+    def width(self, p, p2):
+        return (p2[1] - p[1])
+
+    def area(self, p, p2):
+        l = length(p, p2)
+        w = width(p, p2)
+
+        return (l*w)
+
+    def perimiter(self, p, p2):
+        l = length(p, p2)
+        w = width(p, p2)
+
+        return ((2*l)+(2*w))
+        
+ 
 
 def main():
-    p1 = Point(3.0, 4.0)
+    p12 = Point(3.0, 4.0)
     print("The point ({}, {}) is at a distance of {} from the origin.".format(p1.x, p1.y, p1.abs()))
 
-    p2 = Point(-1.0, 6.5)
+    p22 = Point(-1.0, 6.5)
     print("It is a distance {:.5} away from the point ({}, {}).".format(p1.dist_to(p2), p2.x, p2.y))
 
 
